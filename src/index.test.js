92fa9d3e56693c3d4416b9dd82eb920e35a549d1
Anthony Gauchy <antho2gauchy@gmail.com>
2017-09-26 08:24:47 +0200

    Start of http calls

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 })
 
 describe('index.html', () => {
-    it('should say hello', (done) => {
+    it('should have h1 that says users', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, function(err, widow) {
             const h1 = widow.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello World !");
+            expect(h1.innerHTML).to.equal("Users");
             done();
             window.close();
         });
